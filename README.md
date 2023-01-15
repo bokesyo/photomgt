f4d44aa5c9d9dc525237b620b15e395cd591fec9
Boke Syo <63912729+bokesyo@users.noreply.github.com>
2023-01-15 23:09:28 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# MWG Photo Manager
+
+Cross-platform photo manager with unified indexing (that means create indexing for once, you can browse your photos on any other platforms). The indexing uses relative path, don't worrry if you move photos to new devices!
+
+## Using Scenario
+
+1. I am using MacOS, but I would like to open my album on Windows with `zero cost`. (no re-indexing, sync all my favorates, etc)
+
+2. My album is on a portable drive, I would like to browse my photos using other devices with `zero cost`.
+
+3. I would like to move my albums to another place with `zero cost`.
+
+
+
+
+## Setup
+
+You need to place all your photos in a directory, say `Photos`. Then you put this directory in `Photos` as well. i.e.
+
+-Photos
+    -photomgt
+    -xxxx.jpg
+    -xxxx.jpg
+    -folder1
+        -xxxxx.jpg
+        -xxxxx.jpg
+    
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run Server
+
+```bash
+python manage.py runserver 0.0.0.0:80
+```
+
+## Create Index (only once)
+
+http://localhost/index_all/
+
+## Browse Your Photos
+
+http://localhost/
+
---
+++ myapp/views.py
@@ -0,0 +1,212 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import JsonResponse
+from pathlib import Path
+import os
+import time
+from PIL import Image
+import PIL
+import json
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+PHOTO_BASE_DIR = BASE_DIR.parent
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+print("BASE_DIR", BASE_DIR)
+print("DATA_DIR", DATA_DIR)
+
+
+global_mapping = None
+try:
+    with open(os.path.join(DATA_DIR, 'date_mapping.json'), 'r') as f:
+        global_mapping = json.load(f)
+except:
+    print("Index is not found, please run /index_all!")
+
+
+prev_day_mapping = None
+next_day_mapping = None
+sorted_day = None
+try:
+    with open(os.path.join(DATA_DIR, 'prev_day_mapping.json'), 'r') as f:
+        prev_day_mapping = json.load(f)
+    with open(os.path.join(DATA_DIR, 'next_day_mapping.json'), 'r') as f:
+        next_day_mapping = json.load(f)
+    with open(os.path.join(DATA_DIR, 'sorted_day.json'), 'r') as f:
+        sorted_day = json.load(f)
+except:
+    print("QuickNavigateData is not found, please run /create_navigate_index!")
+
+
+def get_all_file_paths(directory):
+    file_paths = []
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            file_paths.append(os.path.join(dirpath, filename))
+    return file_paths
+
+
+def get_relative_file_paths(root_directory, directory):
+    file_paths = []
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            file_paths.append(os.path.relpath(os.path.join(dirpath, filename), root_directory))
+    return file_paths
+
+
+#获得文件的文件的创建时间 ，使用os.path.getmtime()方法
+def getfile_creat_date(path):
+    ti = time.localtime(os.path.getmtime(path))
+    # 对时间进行格式化
+    file_creat_date = time.strftime('%Y:%m:%d', ti)
+    return file_creat_date
+ 
+
+# 获得照片的拍摄时间，拍摄时间不同于创建时间及修改时间
+def get_mapping(files_names, base_dir):
+    date_item_mapping = {}
+
+    PIL_UnidentifiedImageError = 0
+    SUCCESS = 0
+    EXIF_AttributeError = 0
+    EXIF_KeyError = 0
+    EXIF_TypeError = 0
+    No_EXIF = 0
+    DateValueError = 0
+
+    for item in files_names:
+        if SUCCESS % 1000 == 0:
+            print("UnidentifiedImageError", PIL_UnidentifiedImageError)
+            print("EXIF_AttributeError", EXIF_AttributeError)
+            print("EXIF_KeyError", EXIF_KeyError)
+            print("SUCCESS", SUCCESS)
+            print("No_EXIF", No_EXIF)
+            print("EXIF_TypeError", EXIF_TypeError)
+            print("DateValueError", DateValueError)
+            print('-------')
+        
+        # 定义需要整理的文件格式，支持以下几种格式的照片
+        if item[-3:] == 'jpg' or item[-4:] == 'jpeg' or item[-3:] == 'png' or item[-3:] == 'bmp':
+            try:
+                image = Image.open(item)
+            except PIL.UnidentifiedImageError:
+                PIL_UnidentifiedImageError += 1
+                # 这些已经损坏 不需要索引
+                continue
+            try:
+                file_creat_date = image._getexif()[306]
+            except AttributeError:
+                file_creat_date = getfile_creat_date(item)
+                EXIF_AttributeError += 1
+            except KeyError:
+                file_creat_date = getfile_creat_date(item)
+                EXIF_KeyError += 1
+            except TypeError:
+                file_creat_date = getfile_creat_date(item)
+                EXIF_TypeError += 1
+        else:
+            # 若不是以上指定的格式的照片直接采用getfile_creat_date（）方法获得文件的创建时间
+            file_creat_date = getfile_creat_date(item)
+            No_EXIF += 1
+            # 这些不是图片 不需要索引
+            continue
+        
+        # 获得以日期命名的列表
+        try:
+            int(file_creat_date[0:4])
+            int(file_creat_date[5:7])
+            int(file_creat_date[8:10])
+        except ValueError:
+            DateValueError += 1
+            continue
+        
+        yearmd = f"{file_creat_date[0:4]}-{file_creat_date[5:7]}-{file_creat_date[8:10]}"
+
+        rel_path = os.path.relpath(item, base_dir)
+        # 添加到索引mapping
+        if date_item_mapping.get(yearmd, None) is None:
+            date_item_mapping[yearmd] = [rel_path]
+        else:
+            date_item_mapping[yearmd].append(rel_path)
+        
+        SUCCESS += 1
+    
+    summary = {
+        "PIL_UnidentifiedImageError": PIL_UnidentifiedImageError,
+        "SUCCESS": SUCCESS,
+        "EXIF_AttributeError": EXIF_AttributeError,
+        "EXIF_KeyError": EXIF_KeyError,
+        "EXIF_TypeError": EXIF_KeyError,
+        "No_EXIF": No_EXIF,
+        "DateValueError": DateValueError
+    }
+
+    return date_item_mapping, summary
+
+
+def index_all(request):
+    """
+    进行全面索引
+    """
+    abs_file_paths = get_all_file_paths(PHOTO_BASE_DIR)
+    # print(abs_file_paths[:100])
+    date_mapping, summary = get_mapping(abs_file_paths, PHOTO_BASE_DIR)
+    outfile = os.path.join(DATA_DIR, 'date_mapping.json')
+    print(summary)
+    with open(outfile, 'w') as f:
+        json.dump(date_mapping, f)
+    
+    create_navigate_index(request)
+
+    return HttpResponse(str(summary))
+
+
+def create_navigate_index(request):
+    """
+    实现经济的导航(前一天、后一天)
+    """
+    with open(os.path.join(DATA_DIR, 'date_mapping.json'), 'r') as f:
+        date_mapping = json.load(f)
+    sorted_keys = sorted(date_mapping.keys())
+    next_day_mapping = {}
+    prev_day_mapping = {}
+
+    for today_idx, today in enumerate(sorted_keys):
+        # print(today_idx, today)
+        prev_day_mapping[today] = sorted_keys[max(0, today_idx-1)]
+        next_day_mapping[today] = sorted_keys[min(len(sorted_keys)-1, today_idx+1)]
+    
+    prev_day_mapping_json = os.path.join(DATA_DIR, 'prev_day_mapping.json')
+    next_day_mapping_json = os.path.join(DATA_DIR, 'next_day_mapping.json')
+    sorted_day_json = os.path.join(DATA_DIR, 'sorted_day.json')
+
+    with open(prev_day_mapping_json, 'w') as f:
+        json.dump(prev_day_mapping, f)
+    with open(next_day_mapping_json, 'w') as f:
+        json.dump(next_day_mapping, f)
+    with open(sorted_day_json, 'w') as f:
+        json.dump(sorted_keys, f)
+    
+    return HttpResponse("Create OK.")
+
+def return_property(request, name):
+    abs_img_path = os.path.join(PHOTO_BASE_DIR, name)
+    image_data = open(abs_img_path, "rb").read()
+    return HttpResponse(image_data,content_type="image/png")
+
+def return_avail_dates(request):
+    return JsonResponse(sorted_day, safe=False)
+
+def frontpage(request):
+    return HttpResponseRedirect(f"/day/{sorted_day[-1]}")
+
+def daypage(request, day_string):
+    prev_day = prev_day_mapping[day_string]
+    next_day = next_day_mapping[day_string]
+    imgPathList = global_mapping[day_string]
+    imgPathList = imgPathList
+    imgPathList = imgPathList
+    return render(request, 'daypage.html', {'imgPathList':imgPathList, 'title':day_string, 'prevDay':prev_day, 'nextDay': next_day, 'availableDates': sorted_day})
