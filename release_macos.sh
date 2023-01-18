3366b428f156b67a74886fa9cee5f6157189a56c
Boke Syo <63912729+bokesyo@users.noreply.github.com>
2023-01-18 15:07:08 +0800

    update readme

--- release_macos.sh
+++ release_macos.sh
@@ -6,5 +6,7 @@ rm -rf ./dist/manage/data/*.json
 rm -rf manage.spec 
 rm -rf ./build/
 cp -r ./test_data ./dist/
-zip -r release_macos.zip ./dist/
+zip -r ./release/release_macos.zip ./dist/
+# zip -0 -r ./release/release_windows.zip ./dist/
+# windows 不压缩
 
